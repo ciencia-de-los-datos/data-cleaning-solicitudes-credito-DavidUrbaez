7d9425ceb4e1cb3acaf4c28150d2d00c06702e6e
DavidUrbaez <david.urbaez@smartwires.com>
2023-10-07 12:19:29 -0500

    Problems in Question 4

--- pregunta.py
+++ pregunta.py
@@ -11,7 +11,54 @@ import pandas as pd
 
 
 def clean_data():
-    pass
+    def clean_money_amount(str_amount):
+        if "$" in str_amount:
+            str_amount = str_amount.replace(".00", "").replace(",", "").replace("$ ", "")
+        return int(str_amount)
+
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True,format='mixed')
+
+    df['sexo'] = df['sexo'].str.strip().str.lower()
+
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+
+    df['idea_negocio'] = (
+        df['idea_negocio'].str.lower()
+        .str.replace("-", "_")
+        .str.replace(" ", "_")
+        .str.strip(" _-")
+    )
+
+    df['barrio'] = (
+        df['barrio'].str.lower()
+        .str.strip(" _-")
+        .str.replace(" ", "_").str.replace("-", "_")
+        .str.replace(r"[áéíóúñ¿]", "*", regex=True)
+        .str.replace("belen", "bel*n")
+        .str.replace("andalucia", "andaluc*a")
+        .str.replace("boyaca", "boyac*")
+        .str.replace("'campo_valdes_no._1'", 'campo_vald*s_no.1', regex=False)
+    )
+
+    df['estrato'] = df['estrato'].astype(int)
+
+    df["monto_del_credito"] = df["monto_del_credito"].apply(clean_money_amount).astype(int)
+
+    df['línea_credito'] = (
+        df['línea_credito'].str.lower()
+        .str.strip(" _-")
+        .str.replace(' ', '_').str.replace('-', '_')
+        # .str.replace("soli_diaria", "solidaria")  # ¿?
+    )
+
+    # df = df.drop("comuna_ciudadano", axis=1)
+    df = df.dropna(axis=0)
+    df = df.drop_duplicates()
+
+    return df
 
 
-# clean_data()
+
+clean_data()
